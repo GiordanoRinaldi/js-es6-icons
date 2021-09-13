426a2d6389e6b99ee282b403a215be88c892e264
GiordanoRinaldi <giordanorinaldi0@gmail.com>
2021-09-13 11:13:44 +0200

    ! milestone 1

---
+++ js/script.js
@@ -0,0 +1,124 @@
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
+let icons = [
+
+	{
+	  name: 'apple-alt',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'ice-cream',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'fish',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'lemon',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'hamburger',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'pizza-slice',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'beer',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'glass-whiskey',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'wine-bottle',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'cocktail',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'coffee',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'glass-martini',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'dragon',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'kiwi-bird',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'frog',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'hippo',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'otter',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'horse',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+];
+
+
+
+icons.forEach((elm, i) => {
+    document.getElementById("icons").innerHTML += `
+        <div class="box">
+            <div><i class="${elm.family} ${elm.prefix}${elm.name}"></i></div>
+            <div>${elm.name}</div>
+        </div>`
+})
